c8812c623fb24ae17e84f37a7f9e6c9f7a741758
GreenDole <92329221+GreenDole@users.noreply.github.com>
2022-01-20 05:26:42 +0300

    Task 1-3

---
+++ Task 1/Task 1-3/Task 1-3.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+#include <cstdlib>
+using namespace std;
+
+/**
+* brief функция выводит результат на экран
+* param m,s,q - локальные переменные
+* return 0 - в случае успеха
+*/
+
+int main()
+{
+	double m, s, g;
+	cout << "Hello, print the value: \n"; //Используйте метры
+	cout << "weight: \n";
+	cin >> m;
+	cout << "square: \n";
+	cin >> s;
+	g = 10;
+	double f, p;
+	cout << "find the force of pressure: \n";
+	f = m * g;
+	cout << "f = m * g = \n" << f << endl;
+	cout << "find the pressure: \n";
+	p = f / s;
+	cout << "p = f / s = \n" << p << endl;
+	cout << "answer: \n" << p << endl;
+	return 0;
+
+
+}
