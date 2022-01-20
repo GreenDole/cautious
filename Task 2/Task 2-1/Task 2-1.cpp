a98a85cedead564afcf7dfd0e4e6820ce0421df9
GreenDole <92329221+GreenDole@users.noreply.github.com>
2022-01-20 05:29:10 +0300

    Task 2-1

---
+++ Task 2/Task 2-1/Task 2-1.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+using namespace std;
+
+/**
+*brief функция выводит результат на экран
+* param a,b - локальные переменные
+*return 0 - в случае успеха
+*/
+
+int main()
+{
+    int a, b;
+    cout << "Enter number a:";
+    cin >> a;
+    cout << "Enter number b:";
+    cin >> b;
+
+    cout << "Choose a solution method:\n";
+    cout << "1.Method: selected using third variable C\n";
+    cout << "2.Method: without using the third variable\n";
+    int d;
+    cout << "Print: 1 or 2\n";
+    cin >> d;
+    if (d == 1) {
+        cout << "Used method 1.";
+        int c;
+        c = a;
+        a = b;
+        b = c;
+        cout << "The values have changed!\n";
+        cout << a << "  and  " << b << endl;
+        return 0;
+    }
+    else if (d == 2) {
+        cout << "Used method 2.";
+        a = a + b;
+        b = b - a;
+        b = -b;
+        a = a - b;
+        cout << "The values have changed!\n";
+        cout << a << "  and  " << b << endl;
+        return 0;
+    }
+    else {
+        cout << "Incorrect input, used only nambers 1 or 2";
+        return 0;
+    }
+}
